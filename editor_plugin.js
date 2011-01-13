c23f18a04d174a3fc2cb3fa9be142ef630e0d639
karim.ntic <karim.ntic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-13 20:28:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/reciply/trunk@332215 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- editor_plugin.js
+++ editor_plugin.js
@@ -4,7 +4,7 @@ function reciply() {
 						if (color_option=="orange") image_option="http://www.recip.ly/static/images/widget-add-orange.png";
 						if (color_option=="red") image_option="http://www.recip.ly/static/images/widget-add.png";				
 						}
-    return '<script src="http://www.recip.ly/static/js/jquery-reciply.js" type="text/javascript"></script><div class="reciply-addtobasket-widget" onclick="location.href='+url_option+';" style="cursor:pointer;"><img src='+image_option+'"></div>';
+    return '<script src="http://www.recip.ly/static/js/jquery-reciply.js" type="text/javascript"></script><div class="reciply-addtobasket-widget" onclick="location.href='+""+';" style="cursor:pointer;"><img src='+image_option+'"></div>';
 }
 
 (function() {
