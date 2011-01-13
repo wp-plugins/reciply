6c713069383ecaf5df1f7b7ac1f02f589486838d
craig@techcle.com <craig@techcle.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-13 09:38:43 +0000

    Updated
    git-svn-id: https://plugins.svn.wordpress.org/reciply/trunk@332029 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -1,33 +1,121 @@
 <?php
 /*
-Plugin Name: Purchase Ingredients
+Plugin Name: Reciply Button
 Plugin URI: 
-Description: Purchase Ingredients
+Description: Reciply Button
 Author: Karim Ntic
 Version: 1.0.0
 Author URI: 
 */
 
-function purchase_addbuttons() 
+function reciply_addbuttons() 
 {
-add_filter('mce_external_plugins', "purchase_register");
-add_filter('mce_buttons', 'purchase_add_button', 0);
+add_filter('mce_external_plugins', "reciply_register");
+add_filter('mce_buttons', 'reciply_add_button', 0);
 }
 
-function purchase_add_button($buttons)
+function reciply_add_button($buttons)
 {
-    array_push($buttons, "separator", "purchase");
+    array_push($buttons, "separator", "reciply");
     return $buttons;
 }
 
-function purchase_register($plugin_array)
+function reciply_register($plugin_array)
 {
-    $url = trim(get_bloginfo('url'), "/")."/wp-content/plugins/purchase/editor_plugin.js";
+    $url = trim(get_bloginfo('url'), "/")."/wp-content/plugins/reciply/editor_plugin.js";
 
-    $plugin_array['purchase'] = $url;
+    $plugin_array['reciply'] = $url;
     return $plugin_array;
 }
 
-add_action('init', 'purchase_addbuttons');
+add_action('init', 'reciply_addbuttons');
 
+// create custom plugin settings menu
+add_action('admin_menu', 'reciply_create_menu');
+
+function reciply_create_menu() {
+
+	//create new top-level menu
+	add_menu_page('Reciply Plugin Settings', 'Reciply Settings', 'administrator', __FILE__, 'reciply_settings_page',plugins_url('icon.png', __FILE__));
+
+	//call register settings function
+	add_action( 'admin_init', 'register_mysettings' );
+}
+
+
+function register_mysettings() {
+	//register our settings
+	register_setting( 'reciply-settings-group', 'color' );
+	register_setting( 'reciply-settings-group', 'image' );
+	register_setting( 'reciply-settings-group', 'url' );	
+}
+
+function reciply_settings_page() {
+?>
+<script type="text/javascript">
+function activer(i) {
+//var choice_option= <?php echo json_encode(get_option('choix')); ?>;
+//alert(i);
+if (i==0) { 
+	document.getElementById("red").disabled = false;
+	document.getElementById("orange").disabled = false;
+	document.optionform.image.disabled=true;
+	}
+if (i==1) { 
+	document.optionform.image.disabled=false;
+		document.getElementById("red").disabled = true;
+	document.getElementById("orange").disabled = true;
+	}
+}
+function setpath(v){
+alert(v);
+}
+</script>
+<div class="wrap">
+<h2>Reciply Plugin</h2>
+
+<form name="optionform" method="post" enctype="multipart/form-data" action="options.php">
+    <?php settings_fields('reciply-settings-group'); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row"><input type="radio" name="choix" value="reciply" onclick="javascript:activer(0);" /><b>Reciply Button :</b></th>
+        </tr>	
+        <tr valign="top">
+        <th scope="row">Color</th>
+        <td>
+		<label>Orange<input type="radio" id="orange" name="color" disabled="true" value="orange" /></label>
+		<label>Red<input type="radio" id="red" name="color" disabled="true" value="red" /></label>
+		</td>
+        </tr> 
+        <tr valign="top">
+        <th scope="row"><input type="radio" name="choix" value="custom" onclick="javascript:activer(1);" /><b>Custom Image Button :</b></th>
+        </tr>		
+        <tr valign="top">
+        <th scope="row">Image</th>
+        <td><input type="file" name="image" disabled="true" onchange="javascript:setpath(this.value);" value="<?php echo get_option('image'); ?>" /></td>
+        </tr>
+        </tr>		
+        <tr valign="top">
+        <th scope="row">URL of your Website</th>
+        <td><input type="text" name="url" value="<?php echo get_option('url'); ?>" /></td>
+        </tr>		
+    </table>    
+    <p class="submit">
+    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+</form>
+</div>
+<?php }
+
+add_action('wp_print_scripts','myscript');
+function myscript() {
+?>
+<script type="text/javascript">
+var image_option= <?php echo json_encode(get_option('image')); ?>;
+var color_option= <?php echo json_encode(get_option('color')); ?>;
+var url_option= <?php echo json_encode(get_option('url')); ?>;
+</script>
+<?php
+}
+wp_enqueue_script('myscript','/wp-content/plugins/reciply/editor_plugin.js','');
 ?>
