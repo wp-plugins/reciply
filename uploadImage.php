825919fdf3d625c9d459b0eab66ff10a79b8f30e
karim.ntic <karim.ntic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-15 12:20:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/reciply/trunk@332840 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uploadImage.php
@@ -0,0 +1,35 @@
+<?php
+
+$ref = $_SERVER['HTTP_REFERER'];
+
+// Taille maximum
+$MAX_FILE_SIZE = 150000;
+
+// Dossier de destination du fichier
+$serverpath = "images/";// Path to where images should be uploaded to on the server.
+
+// Tableau array des différents types
+$allowed_types = array("image/bmp", "image/gif", "image/pjpeg", "image/jpeg", "image/jpg", "image/png");
+
+// Variables récupérée par methode POST du formulaires
+$fname = $HTTP_POST_FILES['image']['name'];
+$ftype = $HTTP_POST_FILES['image']['type'];
+$fsize = $HTTP_POST_FILES['image']['size'];
+$ftmp = $HTTP_POST_FILES['image']['tmp_name'];
+$path = "$serverpath$fname";
+
+// Diverses test afin de savoir si :
+// Le format de fichier correspond à notre tableau array
+if(!in_array($ftype, $allowed_types)){$error = 1;}
+
+// La taille du fichier n'est pas dépassée
+if($fize > $MAX_FILE_SIZE){$error = 2;}
+
+// Le fichier n'existe pas déjà
+if(file_exists($serverpath."m_".$fname)){$error = 3;}
+
+// Si tout va bien, c'est bien déroulé
+if(move_uploaded_file($ftmp,''.$serverpath.''.$fname.'')) {$error = 0;}
+
+if($fname!="") header( 'refresh: 0; url='.$ref.'&img='.$path.'&f='.$fname.'&target="_blank"');
+?>
