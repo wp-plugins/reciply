96e24465017f4a0713710a41e914d6b94d82fc02
karim.ntic <karim.ntic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-15 13:36:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/reciply/trunk@332863 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ editor_plugin.js
@@ -0,0 +1,50 @@
+function reciply() {		
+	alert(choix_option);
+	alert(image_option);
+	alert(color_option);
+	var reciplyURL = "http://www.recip.ly";	
+	if(choix_option=="custom") {
+		if ((image_option == "undefined") || (image_option == "") || (image_option == "false")) 
+		{ 
+		color_option = "red";
+		image_option=reciplyURL +"/static/images/widget-add-"+color_option+".png";
+		}
+	}
+	if(choix_option=="reciply") {
+						if((color_option=="") || (color_option==null)) color_option = "red";
+						image_option=reciplyURL +"/static/images/widget-add-"+color_option+".png";
+						}
+    return '<script src='+reciplyURL +'/static/js/jquery-reciply.js" type="text/javascript"></script><div class="reciply-addtobasket-widget"><img src='+image_option+'"></div>';
+}
+
+(function() {
+    tinymce.create('tinymce.plugins.reciply', {
+
+        init : function(ed, url){
+            ed.addButton('reciply', {
+                title : 'Insert Reciply Button',
+                onclick : function() {
+                    ed.execCommand(
+                        'mceInsertContent',
+                        false,
+                        reciply()
+                        );
+                },
+                image: url + "/images/reciply.png"
+            });			
+        },
+
+        getInfo : function() {
+            return {
+                longname : 'Reciply plugin',
+                author : 'The Recip.ly Integration team',
+                authorurl : 'http://integration.recip.ly',
+                infourl : '',
+                version : "1.1.2"
+            };
+        }
+    });
+
+    tinymce.PluginManager.add('reciply', tinymce.plugins.reciply);
+    
+})();
